4656647cade07575d734da28c80ed3759c935290
muturi254 <petermuturi74@gmail.com>
2018-12-18 23:12:45 +0300

    test commit1

--- tribune/settings.py
+++ tribune/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^v(cya#e%ywy+6vearc*0qlgooi_*d$56@ioc&f=+u)r1_13%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
